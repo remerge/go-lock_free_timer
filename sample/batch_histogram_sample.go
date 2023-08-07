731be8c99c6245c5bea5997c4f4bc7a33be967b5
Georgy Buranov <gburanov@gmail.com>
2023-08-07 21:06:45 +0900

    make it compiling

--- sample/batch_histogram_sample.go
+++ sample/batch_histogram_sample.go
@@ -2,17 +2,21 @@ package sample
 
 import (
 	"math"
-	"runtime/metrics"
 	"sync"
+
+	runtimemetrics "runtime/metrics"
+
+	metrics "github.com/rcrowley/go-metrics"
 )
 
 type BatchHistogramSample struct {
 	mu      sync.Mutex
 	buckets []float64 // Inclusive lower bounds, like runtime/metrics.
 	counts  []uint64
+	sum     float64
 }
 
-func NewBatchHistogramSample(buckets []float64) *BatchHistogramSample {
+func NewBatchHistogramSample(buckets []float64) metrics.Sample {
 	// We need to remove -Inf values. runtime/metrics keeps them around.
 	// But -Inf bucket should not be allowed for prometheus histograms.
 	if buckets[0] == math.Inf(-1) {
@@ -29,16 +33,24 @@ func NewBatchHistogramSample(buckets []float64) *BatchHistogramSample {
 	return h
 }
 
-func (h *BatchHistogramSample) update(his *metrics.Float64Histogram, sum float64) {
+func (h *BatchHistogramSample) Clear() {
+	for i := range h.counts {
+		h.counts[i] = 0
+	}
+	h.sum = 0
+}
+
+func (h *BatchHistogramSample) Count() int64 {
+	return int64(len(h.buckets))
+}
+
+func (h *BatchHistogramSample) UpdateFromHistogram(his *runtimemetrics.Float64Histogram, sum float64) {
 	counts, buckets := his.Counts, his.Buckets
 
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
-	// Clear buckets.
-	for i := range h.counts {
-		h.counts[i] = 0
-	}
+	h.Clear()
 	// Copy and reduce buckets.
 	var j int
 	for i, count := range counts {
@@ -47,7 +59,53 @@ func (h *BatchHistogramSample) update(his *metrics.Float64Histogram, sum float64
 			j++
 		}
 	}
-	if h.hasSum {
-		h.sum = sum
-	}
+	h.sum = sum
+}
+
+func (s *BatchHistogramSample) Max() int64 {
+	return 0
+}
+
+func (s *BatchHistogramSample) Mean() float64 {
+	return 0
+}
+
+func (s *BatchHistogramSample) Min() int64 {
+	return 0
+}
+
+func (s *BatchHistogramSample) Percentile(p float64) float64 {
+	return 0
+}
+
+func (s *BatchHistogramSample) Percentiles(_ []float64) []float64 {
+	return nil
+}
+
+func (s *BatchHistogramSample) Size() int {
+	return 0
+}
+
+func (s *BatchHistogramSample) Snapshot() metrics.Sample {
+	return nil
+}
+
+func (s *BatchHistogramSample) StdDev() float64 {
+	return 0
+}
+
+func (s *BatchHistogramSample) Sum() int64 {
+	return 0
+}
+
+func (s *BatchHistogramSample) Update(v int64) {
+
+}
+
+func (s *BatchHistogramSample) Values() []int64 {
+	return nil
+}
+
+func (s *BatchHistogramSample) Variance() float64 {
+	return 0
 }
